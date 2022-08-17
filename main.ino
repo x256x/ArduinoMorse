f0c0d3051772a4c07fda263378c4cd240a243b82
x256x <kaibaseto2007@gmail.com>
2022-08-18 07:52:08 +0900

    ィ゙ゃゾ┛A

--- main.ino
+++ main.ino
@@ -1,85 +1,23 @@
+  /*-------------*/
+ /*<2022, x256x>*/
+/*-------------*/
 /*
  * m_init:  関数 m 初期化用の関数
  *          m_init(ピン番号,周波数(<=32 or 0),スピード(!=0))
  * 
  * m:       モールスで表せる(多分)すべての文字に対応に対応
  *          char*型  Spaceは空でも可
+ * 
+ *m_str:    引数はString型
  */
 void setup(){
-  m_init(13,0,50);
-  //String s = "ah my heart will be hopping";
-  m_str("ah my heart will be hopping");
-  //int l = s.length();
-  /*int j=0;
-  int h=1;
-  String c;
-  c = s.substring(j,h);
-  m(c.c_str());
-  j++;h++;
-  c = s.substring(j,h);
-  m(c.c_str());
-  j++;h++;
-  c = s.substring(j,h);
-  m(c.c_str());*/
-
-  /*int l = s.length();
-  int h=0;
-  int j=1;
-  String c;
-  for(int i=0; i<l; i++){
-      c = s.substring(j,h);
-      m(c.c_str());
-      j++;h++;
-  }*/
-
-
-  /*m("a");
-  m("h");
-  m("");
-  m("m");
-  m("y");
-  m("");
-  m("h");
-  m("e");
-  m("a");
-  m("r");
-  m("t");
-  m("");
-  m("w");
-  m("i");
-  m("l");
-  m("l");
-  m("");
-  m("b");
-  m("e");
-  m("");
-  m("h");
-  m("o");
-  m("p");
-  m("p");
-  m("i");
-  m("n");
-  m("g");
-  m("");
-  m("ア");
-  m("ア");
-  m("コ");
-  m("コ");
-  m("ロ");
-  m("ガ");
-  m("ピ");
-  m("ヨ");
-  m("ン");
-  m("ピ");
-  m("ヨ");
-  m("ン");
-  m("ス");
-  m("ル");
-  m("ン");
-  m("ジ");
-  m("ヤ");
-  m("ー");*/
+	m_init(13,0,500);
+	Serial.begin(9600);
 }
 
-void loop(){
+void loop(){	//シリアルモニタからの文字列をモールスで出力
+	if (Serial.available() > 0){
+		String str1=Serial.readString();
+		m_str(str1);
+	}
 }

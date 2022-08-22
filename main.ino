289a157de2b46c37b33df3a884cb249f09da72fb
x256x <kaibaseto2007@gmail.com>
2022-08-23 07:02:22 +0900

    Copyright

--- main.ino
+++ main.ino
@@ -1,21 +1,12 @@
-  /*-------------*/
- /*<2022, x256x>*/
-/*-------------*/
-/*
- * m_init:  関数 m 初期化用の関数
- *          m_init(ピン番号,周波数(<=32 or 0),スピード(!=0))
- * 
- * m:       モールスで表せる(多分)すべての文字に対応に対応
- *          char*型  Spaceは空でも可
- * 
- *m_str:    引数はString型
- */
+  /*==========================*/
+ /*<Copyright (c) 2022 x256x>*/
+/*==========================*/
 void setup(){
 	m_init(13,0,500);
 	Serial.begin(9600);
 }
 
-void loop(){	//シリアルモニタからの文字列をモールスで出力
+void loop(){
 	if (Serial.available() > 0){
 		String str1=Serial.readString();
 		m_str(str1);

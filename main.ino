82eebe9425558b80cf519fa81bd19ace39cbe6d5
x256x <kaibaseto2007@gmail.com>
2022-08-23 07:04:44 +0900

    A

--- main.ino
+++ main.ino
@@ -2,13 +2,13 @@
  /*<Copyright (c) 2022 x256x>*/
 /*==========================*/
 void setup(){
-	m_init(13,0,500);
-	Serial.begin(9600);
+  m_init(13,0,500);
+  Serial.begin(9600);
 }
 
 void loop(){
-	if (Serial.available() > 0){
-		String str1=Serial.readString();
-		m_str(str1);
-	}
+  if (Serial.available() > 0){
+    String str1=Serial.readString();
+    m_str(str1);
+  }
 }
